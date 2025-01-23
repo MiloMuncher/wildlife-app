a3534c66697eeff753e57a0d00c98c1e9c19d018
MiloMuncher <chongxiehong18@gmail.com>
2025-01-23 17:34:45 +0800

    .

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { Box, AppBar, Toolbar, Typography, Button, IconButton, Drawer, List, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, AppBar, Toolbar, Button, IconButton, Drawer, List, Divider, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import UserContext from '../contexts/UserContext.js';
 
-import MenuIcon from '@mui/icons-material/Menu'
+import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { AccountCircle } from '@mui/icons-material';
 
@@ -12,74 +12,204 @@ function Navbar() {
         localStorage.clear();
         window.location = "/";
     };
+
     const { user, setUser } = useContext(UserContext);
     const isAdmin = user && user.role === 'Admin';
     const isMerchant = user && user.role === 'Merchant';
     const isCustomer = user && user.role === 'Customer';
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setIsOpen(open);
+    };
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" elevation={0} style={{ backgroundColor: 'white', color: 'black' }}>
-                <Toolbar>
+        <Box sx={{ flexGrow: 1 }} style={{ backgroundColor: '#FAF6E3' }}>
+            <AppBar position="static" elevation={0} style={{ backgroundColor: '#FAF6E3', color: 'black' }}>
+                <Toolbar style={{ backgroundColor: '#FAF6E3' }}>
+                    {/* Menu Icon Button */}
                     <IconButton
                         size="large"
                         edge="start"
                         color="inherit"
                         aria-label="menu"
                         sx={{ mr: 2 }}
-                        onClick={() => setIsOpen(true)}
+                        onClick={toggleDrawer(true)}
                     >
                         <MenuIcon />
                     </IconButton>
 
+                    {/* Drawer for the Sidebar Menu */}
+                    <Drawer anchor="left" open={isOpen} onClose={toggleDrawer(false)}>
+                        <Box
+                            sx={{ width: 250 }}
+                            role="presentation"
+                            onClick={toggleDrawer(false)}
+                            onKeyDown={toggleDrawer(false)}
+                        >
+                            <List>
+                                {(isCustomer || !user) && (
+                                    <>
+                                        <ListItem disablePadding>
+                                            <img
+                                                src="src\Images\Home.png"
+                                                alt="logo"
+                                                style={{ height: "25px", width: "25px", padding: 15 }}
+                                            />
+                                            <ListItemButton component={Link} to="/">
+                                                <ListItemText primary="Home" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                        <ListItem disablePadding>
+                                            <img
+                                                src="src\Images\Star.png"
+                                                alt="logo"
+                                                style={{ height: "25px", width: "25px", padding: 15 }}
+                                            />
+                                            <ListItemButton component={Link} to="/categories">
+                                                <ListItemText primary="Support Us" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                        <ListItem disablePadding>
+                                            <img
+                                                src="src\Images\Suitcase.png"
+                                                alt="logo"
+                                                style={{ height: "25px", width: "25px", padding: 15 }}
+                                            />
+                                            <ListItemButton component={Link} to="/contactus">
+                                                <ListItemText primary="How We Operate" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                        <ListItem disablePadding>
+                                            <img
+                                                src="src\Images\Book.png"
+                                                alt="logo"
+                                                style={{ height: "25px", width: "25px", padding: 15 }}
+                                            />
+                                            <ListItemButton component={Link} to="/forum">
+                                                <ListItemText primary="About" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </>
+                                )}
+                                {isMerchant && (
+                                    <ListItem disablePadding>
+                                        <ListItemButton component={Link} to="/merchant/viewevent">
+                                            <ListItemText primary="Merchant" />
+                                        </ListItemButton>
+                                    </ListItem>
+                                )}
+                                {isAdmin && (
+                                    <ListItem disablePadding>
+                                        <ListItemButton component={Link} to="/admin/dashboard">
+                                            <ListItemText primary="Admin" />
+                                        </ListItemButton>
+                                    </ListItem>
+                                )}
+                            </List>
+                            <Divider />
+                            <List>
+                                {user ? (
+                                    <>
+                                        {isCustomer && (
+                                            <>
+                                                <ListItem disablePadding>
 
-                    <Box marginLeft={"1rem"} display={["none", "none", "flex"]} sx={{ flexGrow: 1 }}>
-                        <img src="src\images\logo_uplay.png" alt="logo" style={{ height: "30px", width: "90px", padding: 15     }} />
-                        {(isCustomer || !user) && (
-                            <>
-                                <Button color="inherit" LinkComponent={Link} to='/' sx={{ fontWeight: 'bold' }}>Home</Button>
-                                <Button color="inherit" LinkComponent={Link} to='/categories' sx={{ fontWeight: 'bold' }}>Categories</Button>
-                                <Button color="inherit" LinkComponent={Link} to='/contactus' sx={{ fontWeight: 'bold' }}>Contact Us</Button>
-                                <Button color="inherit" LinkComponent={Link} to='/forum' sx={{ fontWeight: 'bold' }}>Forum</Button>
-                            </>
-                        )}
-                        {isMerchant && (
-                            <Button color="inherit" LinkComponent={Link} to='/merchant/viewevent' sx={{ fontWeight: 'bold' }}>Merchant</Button>
-                        )}
+                                                    <ListItemButton component={Link} to="/cart">
+                                                        <ListItemText primary="Cart" />
+                                                    </ListItemButton>
+                                                </ListItem>
+                                                <ListItem disablePadding>
+                                                    <ListItemButton component={Link} to={`/profile/profile`}>
+                                                        <ListItemText primary={user.name} />
+                                                    </ListItemButton>
+                                                </ListItem>
+                                            </>
+                                        )}
+                                        {isMerchant && (
+                                            <ListItem disablePadding>
+                                                <ListItemButton component={Link} to={`/profile/profile`}>
+                                                    <ListItemText primary={user.name} />
+                                                </ListItemButton>
+                                            </ListItem>
+                                        )}
+                                        <ListItem disablePadding>
+                                            <ListItemButton onClick={logout}>
+                                                <ListItemText primary="Logout" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </>
+                                ) : (
+                                    <>
+                                        <ListItem disablePadding>
+                                            <ListItemButton component={Link} to="/login">
+                                                {user && (
+                                                    <Button onClick={logout} color="inherit" sx={{ fontWeight: 'bold' }}>
+                                                        Logout
+                                                    </Button>
+                                                )}
+                                                {!user && (
+                                                    <>
+                                                        <AccountCircle style={{ color: "black" }} />
+                                                        <Button color="inherit" LinkComponent={Link} to="/login" sx={{ fontWeight: 'bold' }}>
+                                                            Login
+                                                        </Button>
+                                                    </>
+                                                )}
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </>
+                                )}
+                            </List>
+                        </Box>
+                    </Drawer>
+
+                    {/* Logo and Navbar Links */}
+                    <Box style={{ backgroundColor: '#FAF6E3' }} className='content-text' marginLeft={"1rem"} display={["none", "none", "flex"]} sx={{ flexGrow: 1 }}>
+                        <img
+                            src="src/images/logo.png"
+                            alt="logo"
+                            style={{ height: "90px", width: "90px" }}
+                        />
+
                         {isAdmin && (
-                            <Button color="inherit" LinkComponent={Link} to='/admin/dashboard' sx={{ fontWeight: 'bold' }}>Admin</Button>
+                            <Button color="inherit" LinkComponent={Link} to="/admin/dashboard" sx={{ fontWeight: 'bold' }}>
+                                Admin
+                            </Button>
                         )}
                     </Box>
+
+                    {/* Profile and Logout/Login Links */}
                     <>
                         {isCustomer && (
                             <>
                                 <ShoppingCartIcon style={{ color: "black" }} />
-                                <Button color="inherit" LinkComponent={Link} to='/cart' sx={{ fontWeight: 'bold' }}>Cart</Button>
+                                <Button color="inherit" LinkComponent={Link} to="/cart" sx={{ fontWeight: 'bold' }}>
+                                    Cart
+                                </Button>
                                 <AccountCircle style={{ color: 'black' }} />
-                                <Button color="inherit" sx={{ fontWeight: 'bold' }} LinkComponent={Link} to={`/profile/profile`}>{user.name}</Button>
+                                <Button color="inherit" sx={{ fontWeight: 'bold' }} LinkComponent={Link} to={`/profile/profile`}>
+                                    {user.name}
+                                </Button>
                             </>
                         )}
                         {isMerchant && (
                             <>
                                 <AccountCircle style={{ color: 'black' }} />
-                                <Button color="inherit" sx={{ fontWeight: 'bold' }} LinkComponent={Link} to={`/profile/profile`}>{user.name}</Button>
+                                <Button color="inherit" sx={{ fontWeight: 'bold' }} LinkComponent={Link} to={`/profile/profile`}>
+                                    {user.name}
+                                </Button>
                             </>
                         )}
-                        {user && (
-                            <Button onClick={logout} color="inherit" sx={{ fontWeight: 'bold' }}>Logout</Button>
-                        )}
-                        {!user && (
-                            <>
-                                <AccountCircle style={{ color: "black" }} />
-                                <Button color="inherit" LinkComponent={Link} to='/login' sx={{ fontWeight: 'bold' }}>Login</Button>
-                            </>
-                        )}
+
                     </>
                 </Toolbar>
             </AppBar>
         </Box>
-
-
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Parallax } from 'react-parallax';
+import { Background, Parallax } from 'react-parallax';
 import { Container, Box, Paper, Grid, Typography, Button, Divider, Card, CardContent } from '@mui/material'
 import { min } from '@floating-ui/utils';
 
@@ -7,49 +7,18 @@ import { min } from '@floating-ui/utils';
 
 function Home() {
     return (
-
-        <div>
-
+        <div>  
             <Parallax bgImage="src\images\background.png" strength={300}>
-                <div className='content'>
+                <div className='content' >
                     <div className='content-text'>
-                        <Typography variant="h4" style={{ fontWeight: "bold", fontSize: '150px' }}>
-                            Welcome to Uplay!
+                        <Typography variant="h4" style={{ fontWeight: "bold", fontSize: '50px', color: '#FAF6E3' }}>
+                            Rescuing and rehabilitating wildlife for a better tomorrow.
                         </Typography>
-                        <Typography variant="h6" style={{}}>
-                            You play, We'll do the rest
-                        </Typography>
-                    </div>
-                </div>
-            </Parallax>
-            <Parallax bgImage="src\images\bg2.png" strength={200} blur={{min: -5, max: 15 }}>
-                <div className='content'>
-                    <div className='content-text'>
-                        <Typography variant="h6" style={{ color: 'white' }}>
-                            Introducing our
-                        </Typography>
-                        <Typography variant="h4" style={{ fontWeight: "bold", fontSize: '200px', color: 'white' }}>
-                            Experiences
-                        </Typography>
-                    </div>
-                </div>
-            </Parallax>
-            <Parallax bgImage="src\images\background.png" strength={200}>
-                <div className='content'>
-                    <div className='content-text'>
-                    <Typography variant="h4" style={{ fontWeight: "bold", fontSize: '100px' }}>
-                            Partner with us today!
-                        </Typography>
-                        <Typography variant="h6" style={{}}>
-                            Contact us to find out more
-                        </Typography>
-                    </div>
-                </div>
-            </Parallax>
 
-
+                    </div>
+                </div>
+            </Parallax>
         </div>
-
     )
 }
 
